9f7ff064d772195221bf29884a1b4e8541adfe53
unknown <2101333733@qq.com>
2023-08-11 01:51:08 +0800

    卡片中文本的标签值的去除

--- backend/entity/neo4j.py
+++ backend/entity/neo4j.py
@@ -2,6 +2,8 @@ from neo4j_model.db_pool import NEO4j_POOL
 import json
 from models.recognize import Recognize
 import random
+import re
+
 
 def get_list(path):
     with open(path, 'r', encoding='utf-8-sig') as f:
@@ -13,6 +15,7 @@ def get_list(path):
     f.close()
     return list
 
+
 province_list = get_list('entity/data/province.json')
 city_list = get_list('entity/data/city.json')
 province_id_dict = {}
@@ -20,6 +23,7 @@ with open('entity/data/province_idx.txt', 'r', encoding='utf8') as f:
     info = f.readlines()
 for p_ in info:
     province_id_dict[p_.strip().split(',')[1]] = p_.strip().split(',')[0]
+
 
 def entityType(temp):
     if temp in province_list:
@@ -32,6 +36,7 @@ def entityType(temp):
         except Exception:
             t = None
         return t
+
 
 def getDataAndLink(res):
     data = []
@@ -56,6 +61,7 @@ def getDataAndLink(res):
                 link.append(r_)
     return data, link
 
+
 # 大学 -> 专业
 def universitySpecial(university):
     conn = NEO4j_POOL.getConnect()
@@ -63,7 +69,7 @@ def universitySpecial(university):
     # 根据大学名称获取id
     u_id = conn.run("""
         match (n:university) where n.name = "%s" return n.id
-        """ % (university,)).data()[0]["n.id"]
+        """ % (university, )).data()[0]["n.id"]
     # 获取其下的20个专业
     cypher_ = ("""
         match (m:major) where m.fk_university_id = %d return m.name limit %d;
@@ -71,12 +77,27 @@ def universitySpecial(university):
     major = conn.run(cypher_).data()
     data = []
     link = []
-    data.append({ 'name': university, 'symbolSize': 60, 'c': 1, 'type': 'university' })
+    data.append({
+        'name': university,
+        'symbolSize': 60,
+        'c': 1,
+        'type': 'university'
+    })
     for major_dict in major:
-        data.append({ 'name': major_dict['m.name'], 'symbolSize': 60, 'c': 1, 'type': 'major' })
-        link.append({'source': university, 'label': 'HAS', 'target': major_dict['m.name']})
+        data.append({
+            'name': major_dict['m.name'],
+            'symbolSize': 60,
+            'c': 1,
+            'type': 'major'
+        })
+        link.append({
+            'source': university,
+            'label': 'HAS',
+            'target': major_dict['m.name']
+        })
     NEO4j_POOL.free(conn)
     return data, link
+
 
 # 只有实体之一，无关系:
 def onlyOneEntityQuery(entity, num):
@@ -96,6 +117,7 @@ def onlyOneEntityQuery(entity, num):
     data, link = getDataAndLink(res)
     return data, link, type
 
+
 # 一个实体查询  有关系
 def oneOptionAndOneEntityQuery(entity, option, num):
     type = entityType(entity)
@@ -109,9 +131,10 @@ def oneOptionAndOneEntityQuery(entity, option, num):
     """ % (type, option, entity, num))
     conn = NEO4j_POOL.getConnect()
     res = conn.run(cypher_).data()
-    NEO4j_POOL.free(conn)   
+    NEO4j_POOL.free(conn)
 
     return getDataAndLink(res)
+
 
 # 两个实体查询  无关系
 def twoEntityQuery(entity1, entity2, num):
@@ -129,12 +152,13 @@ def twoEntityQuery(entity1, entity2, num):
 
     return getDataAndLink(res)
 
+
 # 判断选定的大学和专业的关系
 def canBack(university, major):
     conn = NEO4j_POOL.getConnect()
     university_id = conn.run("""
         match (n:university) where n.name = "%s" return n.id;
-    """ % (university,)).data()[0]["n.id"]
+    """ % (university, )).data()[0]["n.id"]
     # major_u_id = conn.run("""
     #     match (n:major) where n.name = "%s" and n.fk_university_id = %d return n.fk_university_id;
     # """ % (major, )).data()[0]["n.fk_university_id"]
@@ -147,7 +171,22 @@ def canBack(university, major):
         return [], []
     else:
         # 有关系
-        return [{ 'name': university, 'symbolSize': 60, 'c': 1, 'type': 'university' }, { 'name': major, 'symbolSize': 60, 'c': 1, 'type': 'major' }], [{'source': university, 'label': 'HAS', 'target': major}]
+        return [{
+            'name': university,
+            'symbolSize': 60,
+            'c': 1,
+            'type': 'university'
+        }, {
+            'name': major,
+            'symbolSize': 60,
+            'c': 1,
+            'type': 'major'
+        }], [{
+            'source': university,
+            'label': 'HAS',
+            'target': major
+        }]
+
 
 # 两个实体查询  有关系
 def oneOptionAndtTwoEntityQuery(entity1, option, entity2):
@@ -167,6 +206,7 @@ def oneOptionAndtTwoEntityQuery(entity1, option, entity2):
 
     return getDataAndLink(res)
 
+
 # 获取某一省份下对应的年份信息
 def yearsInfo(province_name):
     # 查询
@@ -178,6 +218,7 @@ def yearsInfo(province_name):
     NEO4j_POOL.free(conn)
     # dict_keys(['n.year'])
     return list(set([k['n.year'] for k in res])), province_list
+
 
 def cateDegreeInfo(province_name, year):
     # 查询
@@ -191,6 +232,7 @@ def cateDegreeInfo(province_name, year):
 
     return list(set([r['n.category']
                      for r in res])), list(set([k['n.degree'] for k in res]))
+
 
 def scoreInfo(province_name, year, category, degree):
     # 查询
@@ -208,13 +250,15 @@ def scoreInfo(province_name, year, category, degree):
     keys = []
     data_detail = {}
     for k_ in data.keys():
-        data_detail[int(k_.split('-')[0])] =  { k_: data[k_] }
+        data_detail[int(k_.split('-')[0])] = {k_: data[k_]}
 
     for key in sorted(data_detail, reverse=True):
         keys.append(list(data_detail[key].keys())[0])
         score.append(list(data_detail[key].values())[0])
 
     return score, keys
+
+
 # 大学 -> 专业
 def universitySpecial(university):
     conn = NEO4j_POOL.getConnect()
@@ -222,7 +266,7 @@ def universitySpecial(university):
     # 根据大学名称获取id
     u_id = conn.run("""
         match (n:university) where n.name = "%s" return n.id
-        """ % (university,)).data()[0]["n.id"]
+        """ % (university, )).data()[0]["n.id"]
     # 获取其下的10个专业
     cypher_ = ("""
         match (m:major) where m.fk_university_id = %d return m.name limit %d;
@@ -230,13 +274,28 @@ def universitySpecial(university):
     major = conn.run(cypher_).data()
     data = []
     link = []
-    data.append({ 'name': university, 'symbolSize': 60, 'c': 1, 'type': 'university' })
+    data.append({
+        'name': university,
+        'symbolSize': 60,
+        'c': 1,
+        'type': 'university'
+    })
     if len(major) != 0:
         for major_dict in major:
-            data.append({ 'name': major_dict['m.name'], 'symbolSize': 60, 'c': 1, 'type': 'major' })
-            link.append({'source': university, 'label': 'HAS', 'target': major_dict['m.name']})
+            data.append({
+                'name': major_dict['m.name'],
+                'symbolSize': 60,
+                'c': 1,
+                'type': 'major'
+            })
+            link.append({
+                'source': university,
+                'label': 'HAS',
+                'target': major_dict['m.name']
+            })
     NEO4j_POOL.free(conn)
     return data, link
+
 
 # match (p:province) - [:REFER] -> (n:major_line) <- [:SET] - (m:major) where p.name = '安徽' and  n.year = '2022' and n.lowScore > '650' return m.name, m.ruanKeScore, m.fk_university_id, n.lowScore limit 5;
 def ScoreRecommend(province, myScore):
@@ -254,20 +313,25 @@ def ScoreRecommend(province, myScore):
     # 处理数据 返回 m.name, m.ruanKeScore, m.fk_university_id, n.lowScore
     universitys_id_name = {}
     for item_dict in res:
-        item_dict["m.ruanKeScore"] = '暂无数据' if item_dict["m.ruanKeScore"] == '' else item_dict["m.ruanKeScore"]
+        item_dict["m.ruanKeScore"] = '暂无数据' if item_dict[
+            "m.ruanKeScore"] == '' else item_dict["m.ruanKeScore"]
         if item_dict["m.fk_university_id"] not in universitys_id_name:
-            universitys_id_name[item_dict["m.fk_university_id"]] = item_dict["m.fk_university_id"]
+            universitys_id_name[item_dict["m.fk_university_id"]] = item_dict[
+                "m.fk_university_id"]
     # 再次查询，获取对应大学的名称
     for i in universitys_id_name.keys():
-        universitys_id_name[i] = conn.run("match (n:university) where n.id = %d return n.name" % (i,)).data()[0]["n.name"]
+        universitys_id_name[i] = conn.run(
+            "match (n:university) where n.id = %d return n.name" %
+            (i, )).data()[0]["n.name"]
     # 添加 res[0] 中每个字典中id对应的名字
     for k in res:
         k['name'] = universitys_id_name[k['m.fk_university_id']]
     NEO4j_POOL.free(conn)
     # 按照 res 中的 lowScore降序排列
-    res.sort(key = lambda t: t['n.lowScore'], reverse=True)
+    res.sort(key=lambda t: t['n.lowScore'], reverse=True)
 
     return res
+
 
 def RankRecommend(province, myRank):
     province_id = province_id_dict[province]
@@ -277,23 +341,28 @@ def RankRecommend(province, myRank):
     """ % (int(province_id), int(myRank)))
     conn = NEO4j_POOL.getConnect()
     res = conn.run(cypher_).data()
-    # 处理数据 返回 m.name, m.ruanKeScore, m.fk_university_id, n.lowScore 
+    # 处理数据 返回 m.name, m.ruanKeScore, m.fk_university_id, n.lowScore
     universitys_id_name = {}
     for item_dict in res:
-        item_dict["m.ruanKeScore"] = '暂无数据' if item_dict["m.ruanKeScore"] == '' else item_dict["m.ruanKeScore"]
+        item_dict["m.ruanKeScore"] = '暂无数据' if item_dict[
+            "m.ruanKeScore"] == '' else item_dict["m.ruanKeScore"]
         if item_dict["m.fk_university_id"] not in universitys_id_name:
-            universitys_id_name[item_dict["m.fk_university_id"]] = item_dict["m.fk_university_id"]
+            universitys_id_name[item_dict["m.fk_university_id"]] = item_dict[
+                "m.fk_university_id"]
     # 再次查询，获取对应大学的名称
     for i in universitys_id_name.keys():
-        universitys_id_name[i] = conn.run("match (n:university) where n.id = %d return n.name" % (i,)).data()[0]["n.name"]
+        universitys_id_name[i] = conn.run(
+            "match (n:university) where n.id = %d return n.name" %
+            (i, )).data()[0]["n.name"]
     # 添加 res[0] 中每个字典中id对应的名字
     for k in res:
         k['name'] = universitys_id_name[k['m.fk_university_id']]
     NEO4j_POOL.free(conn)
     # 按照 res 中的 lowScore降序排列
-    res.sort(key = lambda t: t['n.lowScore'], reverse=True)
+    res.sort(key=lambda t: t['n.lowScore'], reverse=True)
 
     return res
+
 
 # 一个实体  一个标签类型  关系
 def aiTwoEntityQuery(entity_name, entity_type, num, label):
@@ -309,7 +378,7 @@ def aiTwoEntityQuery(entity_name, entity_type, num, label):
         cypher_ = ("""
             match p = (n:%s) - [*1..3] -> (m:%s) where n.name = "%s" and any(rel in relationships(p) where type(rel) = '%s')  return m.name limit 10;
         """ % (type1, entity_type, entity_name, label))
-    else: 
+    else:
         cypher_ = ("""
             match p = (n:%s) <- [*1..3] - (m:%s) where n.name = "%s" and any(rel in relationships(p) where type(rel) = '%s')  return m.name limit 10;
         """ % (type1, entity_type, entity_name, label))
@@ -317,39 +386,50 @@ def aiTwoEntityQuery(entity_name, entity_type, num, label):
     res = conn.run(cypher_).data()
     data = []
     link = []
-    data.append({ 'name': entity_name, 'symbolSize': 60, 'c': 1, 'type': type1 })
+    data.append({'name': entity_name, 'symbolSize': 60, 'c': 1, 'type': type1})
     if len(res) != 0:
         for major_dict in res:
             if major_dict['m.name'] == None:
                 major_dict['m.name'] = entity_type
-            data.append({ 'name': major_dict['m.name'], 'symbolSize': 60, 'c': 1, 'type': entity_type })
-            link.append({'source': entity_name, 'label': label, 'target': major_dict['m.name']})
+            data.append({
+                'name': major_dict['m.name'],
+                'symbolSize': 60,
+                'c': 1,
+                'type': entity_type
+            })
+            link.append({
+                'source': entity_name,
+                'label': label,
+                'target': major_dict['m.name']
+            })
     NEO4j_POOL.free(conn)
     return data, link
 
+
 #随即打乱数组
 def RandomHot(hot):
-    length=len(hot)
+    length = len(hot)
     temp = [i for i in range(length)]
     random.shuffle(temp)
-    arr=[]
+    arr = []
     for i in temp:
         arr.append(hot[i])
     return arr
 
+
 #对数据库中大学专业进行识别并返回
-def content(h,conn):
+def content(h, conn):
     # 连接
-    res='暂无详情'
-    if len(Recognize.recognize(h)['cut_dict'])==0:
-        res='暂无详情'
+    res = '暂无详情'
+    if len(Recognize.recognize(h)['cut_dict']) == 0:
+        res = '暂无详情'
     else:
-        if Recognize.recognize(h)['cut_dict'][0]['label']=='university':
+        if Recognize.recognize(h)['cut_dict'][0]['label'] == 'university':
             cypher = ("""
             match (n:university) where n.name = "%s" return n.intro
             """ % (h))
             res = conn.run(cypher).data()[0]['n.intro']
-        elif Recognize.recognize(h)['cut_dict'][0]['label']=='major':
+        elif Recognize.recognize(h)['cut_dict'][0]['label'] == 'major':
             cypher = ("""
             match (n:major) where n.name = "%s" return n.detail limit 1
             """ % (h))
@@ -357,5 +437,6 @@ def content(h,conn):
         if res[0] == '[':
             res = json.loads(res)
             res = ''.join(res)
-        res = res.replace('\n','').replace('\t','').replace('<p>','').replace('<a>','').replace('\r','').replace('</p>', '').replace('</a>', '').replace('<strong>','').replace('</strong>','').replace('<br />','')
+        res = res.replace('\n', '').replace('\t', '').replace('\r', '').replace('<br />', '')
+        res = re.sub(r'<\/?(p|a|strong)( [^>]*)?>', '', res)
     return res
